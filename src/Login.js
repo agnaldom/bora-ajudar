9411bd2f4432dd4e7c13f751e2c0e18fdab95aea
Agnaldo Marinho <agnaldoneto@ufpa.br>
2018-04-16 16:07:10 -0300

    Adicionando o arquivo para gerencia login

--- src/Login.js
+++ src/Login.js
@@ -1,12 +1,37 @@
 import React, { Component } from 'react'
+import { auth } from './base'
 
-class Login extends Component  {
+class Login extends Component {
+    constructor(props) {
+        super(props)
+
+        this.email = null
+        this.passwd = null
+
+        this.state = {
+            isLoggedIn: false,
+            error: false,
+            isLogging: false
+        }
+
+        this.handleLogin = this.handleLogin.bind(this)
+    }
+    handleLogin() {
+        auth
+            .signInWithEmailAndPassword(this.email.value, this.passwd.value)
+            .then((user) => {
+                console.log('logged in', user)
+            })
+            .catch(error => {
+                console.log('error', error)
+            })
+    }
     render(){
         return (
             <div>
-                <input type='email' />
-                <input type='passwd' />
-                <button onClick={() => console.log('clicou')}>
+                <input type='email' ref={ref => this.email = ref} />
+                <input type='passwd' ref={ref => this.passwd = ref} />
+                <button onClick={this.handleLogin}>
                     Entrar
                 </button>
             </div>
