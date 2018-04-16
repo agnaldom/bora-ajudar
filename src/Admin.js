fc9ab6ed5c7c2d3d682fcb270d1b01ab14fd3eaf
Agnaldo Marinho <agnaldoneto@ufpa.br>
2018-04-16 17:07:47 -0300

    Corrigindo uns erros

--- src/Admin.js
+++ src/Admin.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
-import { Redirect } from 'react-router-dom'
+import { Route, Redirect } from 'react-router-dom'
 
 import { auth } from './base'
+
+const AdminHome = props => <p>Seja bem-vindo!</p>
 
 class Admin extends Component{
     constructor(props) {
@@ -28,7 +30,12 @@ class Admin extends Component{
         if (!this.state.isLoggedIn) {
             return <Redirect to='/login' />
         }
-        return <h1>Admin {JSON.stringify(this.state)}</h1>
+        return(
+            <div>
+                <h1>Painel Administrativo</h1>
+                {/*<Route path='/' component={AdminHome} />*/}
+            </div>
+        )
     }
 }
 
